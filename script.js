3ec1e43587111b64885c2b7b9d71748434ef09ae
Isaque <freitasmelo.mkt.online@gmail.com>
2024-10-25 08:55:13 -0300

    add

--- script.js
+++ script.js
@@ -2,35 +2,42 @@ const container = document.querySelector(".container");
 const qrCodeBtn = document.querySelector("#qr-form button");
 const qrCodeInput = document.querySelector("#qr-form input");
 const qrCodeIMG = document.querySelector("#qr-code img");
-
+const errorMsg = document.createElement("p")
 function generateQrCode() {
     const qrCodeInputValue = qrCodeInput.value;
     if (!qrCodeInputValue) {
-        return;
-    }
-    qrCodeBtn.innerText = "Gerando Código em Instantes...";
-    
-    const qrCodeURL = `https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=${qrCodeInputValue}`;
-    
-    fetch(qrCodeURL)
-        .then(response => response.blob())
-        .then(blob => {
-            const blobURL = URL.createObjectURL(blob);
-            
-            qrCodeIMG.src = blobURL;
-            qrCodeIMG.addEventListener("load", () => {
-                container.classList.add("active");
-                qrCodeBtn.innerText = "QR CODE GERADO!";
+        document.querySelector("#qr-form").appendChild(errorMsg)
+        errorMsg.classList.add("error")
+        errorMsg.innerText = "Escreva o texto ou url"
+        container.classList.add("active")
+    } else {
+        errorMsg.remove()
+        qrCodeBtn.innerText = "Gerando Código em Instantes...";
 
-                const downloadLink = document.createElement("a");
-                downloadLink.href = blobURL;
-                downloadLink.download = "qrcode.png"; // Nome do arquivo que será baixado
-                downloadLink.click();
 
-                // Revogar a URL temporária após o download
-                URL.revokeObjectURL(blobURL);
+        const qrCodeURL = `https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=${qrCodeInputValue}`;
+
+        fetch(qrCodeURL)
+            .then(response => response.blob())
+            .then(blob => {
+
+                const blobURL = URL.createObjectURL(blob);
+
+                qrCodeIMG.src = blobURL;
+                qrCodeIMG.addEventListener("load", () => {
+                    container.classList.add("active");
+                    qrCodeBtn.innerText = "QR CODE GERADO!";
+
+                    const downloadLink = document.createElement("a");
+                    downloadLink.href = blobURL;
+                    downloadLink.download = "qrcode.png"; // Nome do arquivo que será baixado
+                    downloadLink.click();
+
+                    // Revogar a URL temporária após o download
+                    URL.revokeObjectURL(blobURL);
+                });
             });
-        });
+    }
 }
 
 qrCodeBtn.addEventListener("click", () => {
